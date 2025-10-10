d23362926d0ccace37b129b40560a94c3f163537
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2025-10-10 13:49:01 +0000

    push

--- src/main/java/com/example/testEmail/entity/User.java
+++ src/main/java/com/example/testEmail/entity/User.java
@@ -1,6 +1,11 @@
 package com.example.testEmail.service;
 
 
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
 @Entity
 @Table(name = "users")
 public class User implements UserDetails{
--- src/main/java/com/example/testEmail/service/customUserDeatilsService.java
+++ src/main/java/com/example/testEmail/service/customUserDeatilsService.java
@@ -5,4 +5,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class customUserDeatilsService implements UserDetailsService {
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+    }
 }
